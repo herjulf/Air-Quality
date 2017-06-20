ebf65da889e60a77a74df9e1812e0ad2634f039d
Robert Olsson <robert@herjulf.se>
2017-06-20 13:37:29 +0200

    Updated for comparison with mass calc from dust bin.

--- cf1.c
+++ cf1.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+
 /*
 
 2017-05-30
@@ -31,6 +38,29 @@ below 2.5 μm;
 
 */
 
+/* V = 4/3 * 3.141592 * r * r * r = 4.18879 * r * r * r
+
+0.3 um   --> 0.003375 *  4.18879 =  0.014137166
+0.5 um   --> 0.015625 * 4.18879  =  0.0654498
+  1 um   --> 0.125 *  4.18879    =  0.52359
+  2.5 um --> 1.953125 *  4.18879 =  8.181230
+  5 um    --> 15.625 *  4.18879  =  65.44984
+ 10 um    --> 125    *  4.18879  =  523.59
+
+
+ ATM    13   15   17  
+ DB   2274  501   47    5    1    1
+
+ 2274 * 0.014137166 = 32.147915484
+ 501 * 0.0654498    = 32.7903498
+ 47 * 0.52359       = 24.60873
+ 5 * 8.181230       = 40.906150
+ 1 * 65.44984       = 65.44984
+ 1 * 523.59         = 523.59
+
+*/
+
+
 int cf1_to_sat(int cf1)
 {
   int sat = -1;
@@ -42,6 +72,8 @@ int cf1_to_sat(int cf1)
 }
 
 // https://en.wikipedia.org/wiki/Air_quality_index#Computing_the_AQI
+
+#if 0
 
 int aqi[] = {
   0,  //50,
@@ -65,10 +97,218 @@ double pm2_5[] = {0.0, 12.0,
 		  350.5, 500.4
 };
 
-int air_quality_level(int pollutant)
+int air_quality_level(double in, int pollutant)
 {
+
 // I = ((ihi - ilo)/(chi - clo) ) * (c - clo) + ilo
 
+}
 
+#endif
+
+/* V = 4/3 * 3.141592 * r * r * r = 4.18879 * r * r * r
+
+0.3 um   --> 0.003375 *  4.18879 =  0.014137166
+0.5 um   --> 0.015625 * 4.18879  =  0.0654498
+  1 um   --> 0.125 *  4.18879    =  0.52359
+  2.5 um --> 1.953125 *  4.18879 =  8.181230
+  5 um    --> 15.625 *  4.18879  =  65.44984
+ 10 um    --> 125    *  4.18879  =  523.59
+
+
+ ATM    13   15   17  
+ DB   2274  501   47    5    1    1
+*/
+
+double mass(double r, int cnt, double dens)
+{
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass10(double cnt, double dens)
+{
+  double r = 7.5/2;
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass5(double cnt, double dens)
+{
+  double r = (5+2.5)/4;
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass2_5(double cnt, double dens)
+{
+  double r = (2.5+1)/4;
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass1_0(double cnt, double dens)
+{
+  double r = (1+0.5)/4;
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass0_5(double cnt, double dens)
+{
+  double r = (0.5+0.3)/4;
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+double mass0_3(double cnt, double dens)
+{
+  double r = (0.3)/2;
+  cnt = cnt * 2; /* 50% effcieny for PMS5003 */
+  return ((4./3.) * 3.141592 * r * r * r) * cnt * dens;
+}
+
+ex1()
+{
+  double dens = 1;
+  double b0_3, b0_5, b1, b2_5, b5, b10; 
+
+  b0_3 = 2274;
+  b0_5 = 501;
+  b1 = 47;
+  b2_5 = 5;
+  b5 = 1;
+  b10 = 1;
+  
+  printf("0_3=%-6.2f 0_5=%-6.2f 1.0=%-6.2f 2.5=%-6.2f 5=%-6.2f 10=%-6.2f\n", 
+	 mass0_3(b0_3, dens), mass0_5(b0_5, dens), mass1_0(b1, dens), 
+	 mass2_5(b2_5, dens),
+	 mass5(b5, dens),
+	 mass10(b10, dens)
+	 );
+}
+
+struct tag {
+  char buf[40];
+};
+struct tag t[40];
+unsigned int year, mon, day, hour, min, sec;
+struct timeval tv;
+
+unsigned long our_mktime(const unsigned int year0, const unsigned int mon0,
+       const unsigned int day, const unsigned int hour,
+       const unsigned int min, const unsigned int sec)
+{
+	unsigned int mon = mon0, year = year0;
+
+	/* 1..12 -> 11,12,1..10 */
+	if (0 >= (int) (mon -= 2)) {
+		mon += 12;	/* Puts Feb last since it has leap day */
+		year -= 1;
+	}
+
+	return ((((unsigned long)
+		  (year/4 - year/100 + year/400 + 367*mon/12 + day) +
+		  year*365 - 719499
+	    )*24 + hour /* now have hours */
+	  )*60 + min /* now have minutes */
+	)*60 + sec; /* finally seconds */
+}
+
+
+int time_parse(char *buf)
+{
+    int  res = sscanf(buf, 
+		   "%4d-%2d-%2d %2d:%2d:%2d",
+		      &year, &mon, &day, &hour, &min, &sec);
+    
+    return res;
+}
+
+const char *delim = " \t\r,";
+
+int main(int ac, char *av[]) 
+{
+  char buf[BUFSIZ], buf1[BUFSIZ], *p;
+  int i, j, k;
+  int debug;
+  char timebuf[40];
+  int selpos;
+  unsigned int db[6];
+  double ddb[6];
+  unsigned int pma[3];
+  double dpma[6];
+  double dens = 1;
+
+  dens = 1.0;
+
+  int obs;
+
+  ex1();
+
+  debug = 0;
+  
+  for(i=0; i < 6; i++)
+    ddb[i] = 0;
+
+  for(i=0; i < 3; i++)
+    dpma[i] = 0;
+
+  while ( fgets ( buf, BUFSIZ, stdin ) != NULL ) {
+
+    obs++;
+    time_parse(buf);
+    printf("%04u-%02u-%02u %02u:%02u:%02u ",
+	   year, mon, day, hour, min, sec);
+    
+    p = strtok( buf, delim);
+    while(p) {
+      p = strtok( NULL, delim );
+      
+      if (strncmp(p, "ATM", BUFSIZ) == 0) {
+	for(i=0; i < 3; i++) {
+	  p = strtok( NULL, delim );
+	  j = sscanf(p, "%u", &pma[i]);
+	  
+	  dpma[i] += pma[i];
+	  printf("%u ", pma[i]);
+	}
+      }
+
+      if (strncmp(p, "DB", BUFSIZ) == 0) {
+	for(i=0; i < 6; i++) {
+	  p = strtok( NULL, delim );
+	  j = sscanf(p, "%u", &db[i]);
+	  
+	  ddb[i] += db[i];
+	  printf("%u ", db[i]);
+	}
+	printf("\n");
+	break;
+      }
+    }
+  }
+  printf("No Observations = %d\n", obs);
+
+  for(i=0; i < 6; i++) {
+   ddb[i] =  ddb[i]/obs;
+  }
+
+  printf("Bin Averages:\t");
+  for(i=0; i < 6; i++) {
+    printf("%-5.2f ", ddb[i]);
+  }
+  printf("\n");
+
+  for(i=0; i < 3; i++) {
+    dpma[i] = dpma[i]/obs;
+  }
+
+  printf("Particle Density=%-4.2f g/cm**3\n", dens); 
+
+  printf("Mass CALC PM:\tPM0.3=%-5.1f PM0.5=%-5.1f PM1.0=%-5.1f PM2.5=%-5.1f PM5=%-5.1f PM10=%-5.1f\n", 
+	 mass0_3(ddb[0], dens), mass0_5(ddb[1], dens), mass1_0(ddb[2], dens), 
+	 mass2_5(ddb[3], dens),
+	 mass5(ddb[4], dens),
+	 mass10(ddb[5], dens)
+	 );
+
+  printf("Plantower ATM:\tPM1.0=%-5.1f PM2.5=%-5.1f PM10=%-5.1f\n", 
+	 dpma[0], dpma[1], dpma[2]);
 
 }
+
